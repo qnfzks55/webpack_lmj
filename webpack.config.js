a1bea851206085ecdf34a70b3d34ef01af99a604
이명종 <lmj5@naver.com>
2016-12-02 18:08:16 +0900

    16.12.2 커밋

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/index'],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new Webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        }),
+        new Webpack.optimize.OccurrenceOrderPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loaders: ['style', 'css']
+        }]
+    }
+};
